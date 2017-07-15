cb8536bfaca58497b61d16bc1b3f221e89501eb2
JeongHyunLee <nomar0107@gmail.com>
2017-07-16 00:22:05 +0900

    ㄴㅁㅇㄹ
    ㅁㄴㅇㄹ

--- app/MainScreens/Vote/index.js
+++ app/MainScreens/Vote/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 
 
-
 import {
     Text,
     View,
@@ -17,55 +16,55 @@ import {
 import Button from "react-native-button";
 
 
-
-
-
-export default class index extends Component{
+export default class index extends Component {
     constructor(props) {
+
         super(props);
         this.state = {
-            randomPost:"",
+            randomPost: "",
+            fired: false,
+            msg: ""
         };
     }
 
-    async pressFireButton(post_id,written_by) {
-        this.setState({showProgress: true})
-        this.setState({fired: true})
-        try {
-            let response = await fetch('http://54.162.160.91/api/post/fire', {
+    async pressFireButton(post_id, written_by) {
+        console.log("post : "+post_id);
+        console.log("wrttenBy : "+written_by);
+
+        AsyncStorage.getItem("token").then((value) => {
+            fetch('http://54.162.160.91/api/post/fire', {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
+                    'x-access-token': value
                 },
                 body: JSON.stringify({
 
-                    post_id : post_id ,
-                    writtenBy : written_by
+                    post_id: post_id,
+                    writtenBy: written_by
 
                 })
-            });
-            let res = await response.json();
-            if (response.status >= 200 && response.status < 300) {
-                //Handle success
-                let accessToken = res.token;
-                console.log(accessToken);
-                //On success we will store the access_token in the AsyncStorage
-                this.storeToken(accessToken);
+            })
+                .then((response) => response.json())
+                .then(responseData => {
+                    this.setState({
+                        fired:true,
+                        msg:responseData
+                    });
+                    console.log(responseData);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        })
 
-            } else {
 
-                let error = res;
-                throw error;
-            }
-        } catch (error) {
-            this.setState({error: error.message});
-            console.log(error);
-            this.setState({showProgress: false});
-        }
     }
 
+
     componentDidMount() {
+        alert("!");
         AsyncStorage.getItem("token").then((value) => {
             fetch('http://54.162.160.91/api/post/random', {
                 method: 'GET',
@@ -78,12 +77,11 @@ export default class index extends Component{
                 .then((response) => response.json())
                 .then(responseData => {
 
-
+                    console.log("!!!");
+                    console.log(responseData.message);
                     this.setState({
-                        randomPost:responseData.randomPost
+                        randomPost: responseData.message
                     });
-                    console.log(this.state.randomPost)
-
 
 
 
@@ -97,18 +95,21 @@ export default class index extends Component{
     }
 
 
-
-    render(){
+    render() {
 
         let button = null;
-        if(this.state.fired === false){
+        if (this.state.fired === false) {
             button = <Button
-                onPress={() => this.pressFireButton(this.state.randomPost._id,this.state.randomPost.writtenBy)}
+                onPress={() => this.pressFireButton(this.state.randomPost.post_id, this.state.randomPost.writtenBy)}
                 style={{fontSize: 10, color: 'black', padding: 20, letterSpacing: 3}}>
                 Fire!
             </Button>
         }
-        return(
+        else {
+
+            button = <Text>{this.state.msg}</Text>
+        }
+        return (
 
             <View style={styles.container}>
                 <Text>{this.state.randomPost._id}</Text>
@@ -128,10 +129,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
 
-        justifyContent:'center',
-        alignItems:'center',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-
 
 
 });
