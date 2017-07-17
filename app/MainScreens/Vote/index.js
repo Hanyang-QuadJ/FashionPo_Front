c71f2c7ea39714871a63cdf7bf066edd2534cc6d
ShinHyunJong <kashiashin@gmail.com>
2017-07-17 17:13:41 +0900

    123123

--- app/MainScreens/Vote/index.js
+++ app/MainScreens/Vote/index.js
@@ -1,72 +1,90 @@
 import React, {Component} from 'react'
 
 
+
 import {
     Text,
     View,
     StatusBar,
     StyleSheet,
     Navigator,
-    Image,
+    Dimensions,
+    findNodeHandle,
+    TouchableHighlight,
+
     TouchableOpacity,
     AsyncStorage,
-
+    Image,
 
 } from 'react-native'
 import Button from "react-native-button";
+import Icon from 'react-native-vector-icons/Ionicons'
+import BlurView from 'react-native-blur';
 
 
-export default class index extends Component {
+
+
+
+
+export default class index extends Component{
     constructor(props) {
-
         super(props);
         this.state = {
-            randomPost: "",
-            fired: false,
-            msg: ""
+            randomPost:"",
+            pressStatus: false,
+            blur:null,
+
         };
     }
 
-    async pressFireButton(post_id, written_by) {
-        console.log("post : "+post_id);
-        console.log("wrttenBy : "+written_by);
+    makeBlur (blur) {
+        if(blur === 0) this.setState({blur:100})
+        else this.setState({blur:0})
+    }
 
-        AsyncStorage.getItem("token").then((value) => {
-            fetch('http://54.162.160.91/api/post/fire', {
+    makeNotBlur() {
+        this.setState
+    }
+
+
+    async pressFireButton(post_id,written_by) {
+        this.setState({showProgress: true})
+        this.setState({fired: true})
+        try {
+            let response = await fetch('http://54.162.160.91/api/post/fire', {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
-                    'x-access-token': value
                 },
                 body: JSON.stringify({
 
-                    post_id: post_id,
-                    writtenBy: written_by
+                    post_id : post_id ,
+                    writtenBy : written_by
 
                 })
-            })
-                .then((response) => response.json())
-                .then(responseData => {
-                    console.log("!!!!");
-                    console.log(responseData);
-                    this.setState({
-                        fired:true,
-                        msg:responseData.message
-                    });
-                    console.log(responseData);
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-        })
+            });
+            let res = await response.json();
+            if (response.status >= 200 && response.status < 300) {
+                //Handle success
+                let accessToken = res.token;
+                console.log(accessToken);
+                //On success we will store the access_token in the AsyncStorage
+                this.storeToken(accessToken);
 
+            } else {
 
+                let error = res;
+                throw error;
+            }
+        } catch (error) {
+            this.setState({error: error.message});
+            console.log(error);
+            this.setState({showProgress: false});
+        }
     }
 
-
     componentDidMount() {
-
         AsyncStorage.getItem("token").then((value) => {
             fetch('http://54.162.160.91/api/post/random', {
                 method: 'GET',
@@ -79,11 +97,12 @@ export default class index extends Component {
                 .then((response) => response.json())
                 .then(responseData => {
 
-                    console.log("!!!");
-                    console.log(responseData.message);
+
                     this.setState({
-                        randomPost: responseData.message
+                        randomPost:responseData.randomPost
                     });
+                    console.log(this.state.randomPost)
+
 
 
 
@@ -97,26 +116,53 @@ export default class index extends Component {
     }
 
 
-    render() {
 
-        let button = null;
-        if (this.state.fired === false) {
-            button = <Button
-                onPress={() => this.pressFireButton(this.state.randomPost._id, this.state.randomPost.writtenBy)}
-                style={{fontSize: 10, color: 'black', padding: 20, letterSpacing: 3}}>
-                Fire!
-            </Button>
-        }
-        else {
+    render(){
 
-            button = <Text>{this.state.msg}</Text>
-        }
-        return (
+        // let button = null;
+        // if(this.state.fired === false){
+        //     button = <Button
+        //         onPress={() => this.pressFireButton(this.state.randomPost._id,this.state.randomPost.writtenBy)}
+        //         style={{fontSize: 10, color: 'black', padding: 20, letterSpacing: 3}}>
+        //         Fire!
+        //     </Button>
+        // }
+        return(
 
             <View style={styles.container}>
-                {/*<Text>{this.state.randomPost._id}</Text>*/}
 
-                {/*{button}*/}
+
+                <View style={styles.navbar}>
+                    <TouchableOpacity><Icon name="ios-camera" size={24} color="white"/></TouchableOpacity>
+                    <Text style={styles.titleText}>Fashion Po</Text>
+                    <TouchableOpacity><Icon name="md-arrow-round-forward" size={24} color="white"/></TouchableOpacity>
+                </View>
+
+                <View style={styles.pictureArea}>
+                    <TouchableOpacity onPress={()=>this.makeBlur(this.state.blur)}>
+                        <Image source={require('../../Img/vote.png')} style={styles.profileImage}  blurRadius={this.state.blur} />
+                    </TouchableOpacity>
+
+
+                    {/*<View style={styles.innerFrame}>*/}
+                        {/*<View style={styles.padding}>*/}
+
+                                {/*<Text style={styles.userName}>*/}
+                                    {/*Kendall Jenner*/}
+                                {/*</Text>*/}
+
+                            {/*<View style={styles.tagsArea}>*/}
+                                {/*<Text style={styles.tagsText}>#Valencia</Text>*/}
+                                {/*<Text style={styles.tagsText}>#Fashion</Text>*/}
+                                {/*<Text style={styles.tagsText}>#Pants</Text>*/}
+                                {/*<Text style={styles.tagsText}>#Shoes</Text>*/}
+                                {/*<Text style={styles.tagsText}>#Shorts</Text>*/}
+                            {/*</View>*/}
+                        {/*</View>*/}
+                    {/*</View>*/}
+                </View>
+
+
             </View>
 
 
@@ -126,16 +172,94 @@ export default class index extends Component {
         );
     }
 }
+const {width, height} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection:'column',
 
-        justifyContent: 'center',
-        alignItems: 'center',
+    },
+    navbar: {
+        paddingTop: 20,
+        height: 64,
+        backgroundColor: "#ff5733",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: '#DDDDDD',
+        paddingHorizontal: 12,
+        flexDirection: 'row', // step 1
+        justifyContent: 'space-between', // step 2
+        alignItems: 'center', // step 3
+    },
+    leftText: {
+        color: 'white',
+    },
+    titleText: {
+        fontWeight: '600',
+        color: 'white',
+    },
+    rightText: {
+        color: 'white',
+    },
+    pictureArea:{
+        flex:1,
+
+
+    },
+    profileImage:{
+
+        width: width.width,
+        height: 700,
+        resizeMode:"cover",
+
+    },
+    innerFrame: {
+
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        backgroundColor:"rgba(0,0,0,0.2)"
+
+    },
+    absolute: {
+        position: "absolute",
+        top: 0, left: 0, bottom: 0, right: 0,
+    },
+
+    tags:{
+        flex:1,
+        flexDirection:'row',
+        backgroundColor:'transparent',
+
+    },
+    padding:{
+        padding:20,
+        flex:1,
+        flexDirection:"column",
+    },
+    userNameArea:{
+        flex:0.5,
+    },
+    userName:{
+        fontSize:17,
+        color:"white",
+        fontWeight:"500",
+        paddingBottom:5,
+    },
+    tagsText:{
+        fontSize:15,
+        color:"white",
+        marginRight:5,
+    },
+    tagsArea:{
+        flex:1,
+        flexDirection:"row",
+
     },
 
 
+
+
 });
-
-
--- app/config/Router.js
+++ app/config/Router.js
@@ -58,7 +58,8 @@ export const AuthStack = StackNavigator({
 
         }
 
-    }
+    },
+
 
 
 });
